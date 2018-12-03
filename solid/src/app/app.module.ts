c6fd99f396a558d6b1a804ec05a0f74068129be4
Artur Kuznetsov <Artur.Kuznetsov@solidexpert.local>
2018-12-03 14:48:28 -0800

    footer component add

--- solid/src/app/app.module.ts
+++ solid/src/app/app.module.ts
@@ -8,12 +8,13 @@ import { DevelopmentPageComponent } from './pages/development-page/development-p
 import { ProductsPageComponent } from './pages/products-page/products-page.component';
 import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
 import { ContactUsPageComponent } from './pages/contact-us-page/contact-us-page.component';
+import { CvGeneratorPageComponent } from './pages/cv-generator-page/cv-generator-page.component';
+import { FooterComponent } from './ui/footer/footer.component';
 
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { SliderComponent } from './ui/slider/slider.component';
 import { RouterModule } from '@angular/router';
 import { route } from './routing';
-import { CvGeneratorPageComponent } from './pages/cv-generator-page/cv-generator-page.component';
 
 
 @NgModule({
@@ -22,11 +23,12 @@ import { CvGeneratorPageComponent } from './pages/cv-generator-page/cv-generator
     HomePageComponent,
     HeaderComponent,
     DevelopmentPageComponent,
-    SliderComponent,
     ProductsPageComponent,
     ProjectsPageComponent,
     ContactUsPageComponent,
-    CvGeneratorPageComponent
+    CvGeneratorPageComponent,
+    SliderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
