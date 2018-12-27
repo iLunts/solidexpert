8e43e9b40d7ee3fcbbd65416013ece5e5113c0b9
Artur Kuznetsov <artkuznetsov191@gmail.com>
2018-12-27 14:09:39 +0200

    slider component

--- solid/src/app/ui/slider/slider.component.ts
+++ solid/src/app/ui/slider/slider.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.less']
+  styleUrls: ['./slider.component.less'],
+  encapsulation: ViewEncapsulation.None
 })
 
 
@@ -17,7 +18,8 @@ export class SliderComponent implements OnInit {
       use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web
       sites still in their infancy. Various versions have evolved over the years, sometimes by accident,
       sometimes on purpose (injected humour and the like).`,
-      index: 1,
+      name: 'Joe Spencer',
+      position: 'director',
       company: 'fotoBel'
     },
     {
@@ -26,7 +28,8 @@ export class SliderComponent implements OnInit {
       of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
       more-or-less normal distribution of letters, as opposed to using 'Content here, content here',
       making it look like readable English. `,
-      index: 2,
+      name: 'Marvin Gutierrez',
+      position: ' manager',
       company: 'SolidCRM'
     },
     {
@@ -39,7 +42,8 @@ export class SliderComponent implements OnInit {
       of a page when looking at its layout. The point of using Lorem Ipsum is that it has a
       more-or-less normal distribution of letters, as opposed to using 'Content here, content here',
       making it look like readable English. `,
-      index: 3,
+      name: 'Fernando Sullivan',
+      position: 'chief ',
       company: 'Salesinteract'
     }
   ];
