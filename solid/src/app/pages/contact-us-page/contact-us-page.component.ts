3e9c722e841e1fb490c4f680b824f1755cf737c2
Artur Kuznetsov <artkuznetsov191@gmail.com>
2019-01-05 12:45:00 +0200

    changes form

--- solid/src/app/pages/contact-us-page/contact-us-page.component.ts
+++ solid/src/app/pages/contact-us-page/contact-us-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-us-page',
@@ -7,9 +8,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./contact-us-page.component.less']
 })
 export class ContactUsPageComponent implements OnInit {
+
   requestForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) { }
+  userName = '';
+  userPhone = '';
+  userEmail = '';
+  userMessage = '';
+
+  constructor(private formBuilder: FormBuilder, protected http: HttpClient) { }
 
   ngOnInit() {
     this.requestForm = this.formBuilder.group({
@@ -32,5 +39,28 @@ export class ContactUsPageComponent implements OnInit {
 
     alert('Thx!!! :-)');
     this.requestForm.reset();
+  }
+  
+  send() {
+
+    const body = new HttpParams()
+    //  .set('is_company',"false")
+     .set('userName', this.userName)
+     .set('userPhone', this.userPhone)
+     .set('userEmail', this.userEmail)
+     .set('userMessage', this.userMessage)
+     .set('return_url', 'http://solidexpert.ltd')
+     .set('web_form_key', '3a51d614-03b1-43b9-bca2-a3ff717ca08e')
+     .set('notify_list', '61560477-f93c-11e8-94e2-6e81647443e7')
+     .set('managers_list', '61560477-f93c-11e8-94e2-6e81647443e7,53a72559-aa04-4ae5-89e2-898cb8fc8f23')
+     .set('share_type', '0');
+
+      return this.http.post('http://office.solidexpert.ltd/products/crm/httphandlers/webtoleadfromhandler.ashx',
+     body.toString(),
+     {
+       headers: new HttpHeaders()
+         .set('Content-Type', 'application/x-www-form-urlencoded')
+     }
+   ).subscribe(x => {});
   }
 }
