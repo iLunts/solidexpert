0c9f582448aabe46726d7e009ed9e5a85f8bc78e
Siarhei Shchypanau <breate.by@gmail.com>
2019-01-08 15:09:57 +0300

    Merge branch 'master' of https://github.com/iLunts/solidexpert

--- solid/src/app/pages/contact-us-page/contact-us-page.component.ts
+++ solid/src/app/pages/contact-us-page/contact-us-page.component.ts
@@ -8,7 +8,6 @@ import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
   styleUrls: ['./contact-us-page.component.less']
 })
 export class ContactUsPageComponent implements OnInit {
-
   requestForm: FormGroup;
 
   userName = '';
@@ -16,51 +15,78 @@ export class ContactUsPageComponent implements OnInit {
   userEmail = '';
   userMessage = '';
 
-  constructor(private formBuilder: FormBuilder, protected http: HttpClient) { }
+  constructor(private formBuilder: FormBuilder, protected http: HttpClient) {}
 
   ngOnInit() {
     this.requestForm = this.formBuilder.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      telephone: ['', ],
-      privacy: ['', Validators.requiredTrue],
+      userMessage: [''],
+      telephone: [''],
+      privacy: ['', Validators.requiredTrue]
     });
   }
 
   // convenience getter for easy access to form fields
-  get f() { return this.requestForm.controls; }
+  get f() : any {
+    return this.requestForm.controls;
+  }
 
   onSubmit() {
-
     // stop here if form is invalid
     if (this.requestForm.invalid) {
       return;
     }
 
-    alert('Thx!!! :-)');
+    alert('Thanks alot');
     this.requestForm.reset();
   }
-  
+
   send() {
 
-    const body = new HttpParams()
-    //  .set('is_company',"false")
-     .set('userName', this.userName)
-     .set('userPhone', this.userPhone)
-     .set('userEmail', this.userEmail)
-     .set('userMessage', this.userMessage)
-     .set('return_url', 'http://solidexpert.ltd')
-     .set('web_form_key', '3a51d614-03b1-43b9-bca2-a3ff717ca08e')
-     .set('notify_list', '61560477-f93c-11e8-94e2-6e81647443e7')
-     .set('managers_list', '61560477-f93c-11e8-94e2-6e81647443e7,53a72559-aa04-4ae5-89e2-898cb8fc8f23')
-     .set('share_type', '0');
+    const value = this.requestForm.value;
 
-      return this.http.post('http://office.solidexpert.ltd/products/crm/httphandlers/webtoleadfromhandler.ashx',
-     body.toString(),
-     {
-       headers: new HttpHeaders()
-         .set('Content-Type', 'application/x-www-form-urlencoded')
-     }
-   ).subscribe(x => {});
+    console.log(value);
+
+    const arr = value.name.split(' ');
+
+    let firstName = '';
+    let lastName = '';
+    if (arr.length > 1) {
+      firstName = arr[0];
+      lastName = arr[1];
+    } else {
+      firstName = arr[0];
+      lastName = '';
+    }
+
+    const body = new HttpParams()
+      .set('is_company', 'false')
+      .set('firstName', firstName)
+      .set('lastName', lastName)
+      .set('contactInfo_Phone_1', value.telephone)
+      .set('contactInfo_Email_1', value.email)
+      .set('message', this.userMessage)
+      .set('return_url', 'http://solidexpert.ltd')
+      .set('web_form_key', '3a51d614-03b1-43b9-bca2-a3ff717ca08e')
+      .set('notify_list', '61560477-f93c-11e8-94e2-6e81647443e7')
+      .set(
+        'managers_list',
+        '61560477-f93c-11e8-94e2-6e81647443e7,53a72559-aa04-4ae5-89e2-898cb8fc8f23'
+      )
+      .set('share_type', '0');
+
+    return this.http
+      .post(
+        'http://office.solidexpert.ltd/products/crm/httphandlers/webtoleadfromhandler.ashx',
+        body.toString(),
+        {
+          headers: new HttpHeaders().set(
+            'Content-Type',
+            'application/x-www-form-urlencoded'
+          )
+        }
+      )
+      .subscribe(x => {});
   }
 }
