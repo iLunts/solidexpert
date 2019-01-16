7b33adc10a1593d6ac598082812d9c6752351231
Artur Kuznetsov <Artur.Kuznetsov@solidexpert.local>
2019-01-15 18:21:50 -0800

    cv-list add list cv

--- solid/src/app/pages/cv-list-page/cv-list-page.component.ts
+++ solid/src/app/pages/cv-list-page/cv-list-page.component.ts
@@ -6,10 +6,154 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cv-list-page.component.less']
 })
 export class CvListPageComponent implements OnInit {
+  developers = [
+    {
+      firstName: 'Jon',
+      lastName: 'Guerrero',
+      specialization: 'Android',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book.`
+    },
+    {
+      firstName: 'Sam',
+      lastName: 'Shadel',
+      specialization: 'Java',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type
+specimen book.`
+    },
+    {
+      firstName: 'Martin',
+      lastName: 'Fusco',
+      specialization: '.Net',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type
+specimen book.`
+    },
+    {
+      firstName: 'Jess',
+      lastName: 'Bundy',
+      specialization: 'Front End',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book.`
+    },
+    {
+      firstName: 'Nicko',
+      lastName: 'Sarkisian',
+      specialization: 'Designer',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book.`
+    },
+    {
+      firstName: 'Goddart',
+      lastName: 'Krichmar',
+      specialization: 'Sale manager',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book.`
+    },
+    {
+      firstName: 'Mill',
+      lastName: 'Maalouf',
+      specialization: 'Phyton',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book.`
+    },
+    {
+      firstName: 'Stearne',
+      lastName: 'Ayling',
+      specialization: 'JavaScript',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book.`
+    }
+    {
+      firstName: 'Jon',
+      lastName: 'Guerrero',
+      specialization: 'Android',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book.`
+    },
+    {
+      firstName: 'Sam',
+      lastName: 'Shadel',
+      specialization: 'Java',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type
+specimen book.`
+    },
+    {
+      firstName: 'Martin',
+      lastName: 'Fusco',
+      specialization: '.Net',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type
+specimen book.`
+    },
+    {
+      firstName: 'Jess',
+      lastName: 'Bundy',
+      specialization: 'Front End',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book.`
+    },
+    {
+      firstName: 'Nicko',
+      lastName: 'Sarkisian',
+      specialization: 'Designer',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book.`
+    },
+    {
+      firstName: 'Goddart',
+      lastName: 'Krichmar',
+      specialization: 'Sale manager',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book.`
+    },
+    {
+      firstName: 'Mill',
+      lastName: 'Maalouf',
+      specialization: 'Phyton',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book.`
+    },
+    {
+      firstName: 'Stearne',
+      lastName: 'Ayling',
+      specialization: 'JavaScript',
+      Info: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type
+          specimen book.`
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- solid/src/app/routing.ts
+++ solid/src/app/routing.ts
@@ -7,6 +7,7 @@ import { ServicesPageComponent } from './pages/services-page/services-page.compo
 import { ContactUsPageComponent } from './pages/contact-us-page/contact-us-page.component';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { ProductsListPageComponent } from './pages/products-list-page/products-list-page.component';
+import { CvListPageComponent } from './pages/cv-list-page/cv-list-page.component';
 
 
 const routes: Routes = [
@@ -17,6 +18,8 @@ const routes: Routes = [
     { path: 'services', component: ServicesPageComponent },
     { path: 'contact', component: ContactUsPageComponent },
     { path: '', component: MainPageComponent },
+    { path: 'cv', component: CvListPageComponent }
+
     // { path: 'order', component: HomePageComponent, pathMatch: 'full' },
     // { path: '**', component: HomePageComponent },
 ];
