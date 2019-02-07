54e1d0e0ecb19027ead1169824f5ccfd58d15f5c
Artur Kuznetsov <Artur.Kuznetsov@solidexpert.local>
2019-02-07 14:41:18 -0800

    prod details add animation

--- solid/src/app/ui/poducts-details/block-fourth/block-fourth.component.ts
+++ solid/src/app/ui/poducts-details/block-fourth/block-fourth.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { NgwWowService } from 'ngx-wow';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-block-fourth',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlockFourthComponent implements OnInit {
 
-  constructor() { }
+  private wowSubscription: Subscription;
+
+  constructor(private router: Router, private wowService: NgwWowService) {
+    this.router.events.pipe (filter(event => event instanceof NavigationEnd)).subscribe(event => {
+      // Reload WoW animations when done navigating to page,
+      // but you are free to call it whenever/wherever you like
+      this.wowService.init();
+
+    });
+
+  }
 
   ngOnInit() {
+    // you can subscribe to WOW observable to react when an element is revealed
+    this.wowSubscription = this.wowService.itemRevealed$.subscribe(
+      (item: HTMLElement) => {
+        // do whatever you want with revealed element
+      });
   }
+
+  // ngOnDestroy() {
+  //   // unsubscribe (if necessary) to WOW observable to prevent memory leaks
+  //   this.wowSubscription.unsubscribe();
+  // }
 
 }
